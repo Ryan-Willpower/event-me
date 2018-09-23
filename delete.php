87d6fa8b717551c6a7440bf30162b8e13ecc778c
Ryan-Willpower <surasak.c.work@gmail.com>
2018-09-24 00:34:28 +0700

    refactor file sturcture

--- delete.php
+++ delete.php
@@ -1,12 +1,2 @@
 <?php
-require('php/session.php');
-require('php/connect_sql.php');
-$admin = $_SESSION['role'] ?? 'None';
-$title = $_GET['title'] ?? 'None';
-
-if ($admin == 'admin') {
-  $query = "DELETE FROM `event` WHERE title='$title'";
-  $mysql->query($query);
-}
-
-header('location: index.php');
+require('php/admin/delete.php');
