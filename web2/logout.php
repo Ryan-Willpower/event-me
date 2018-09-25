fdddfaee281bef2676d04c57af53201923d54517
Ryan-Willpower <surasak.c.work@gmail.com>
2018-09-25 20:38:03 +0700

    remove unnecessary file

--- web2/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-
-echo "<script language='javascript'>alert('Logout successfully.')</script>";
-echo "<meta http-equiv='refresh' content='0;url=login.php' />";
- ?>
